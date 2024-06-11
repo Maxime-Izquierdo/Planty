61a1a4520afdff51b9e3f603d91762c17cace902
Maxime Izquierdo <mxm.izquierdo@gmail.com>
2024-06-11 06:03:06 +0200

    Troisième commit avec l'ajout du hook pour le lien Admin

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -11,8 +11,23 @@ function planty_enqueue_styles() {
     
     
     wp_enqueue_style( 
-        'planty-child-style',
+        'planty-style',
         get_stylesheet_directory_uri() . '/style.css',
         array('astra-style') 
     );
 }
+
+function hide_admin_link_for_non_logged_in_users() {
+    if ( ! is_user_logged_in() ) {
+        echo '<style type="text/css">
+                .ast-builder-layout-element.ast-header-html-2 {
+                    display: none;
+                }
+              </style>';
+    }
+}
+add_action('wp_head', 'hide_admin_link_for_non_logged_in_users');
+
+
+
+?>
